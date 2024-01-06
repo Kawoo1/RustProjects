2172cded53d1d21c9b4e12a7f9ffe716d7d5c749
Kyle Shanahan <143049238+Kawoo1@users.noreply.github.com>
2024-01-05 19:47:40 -0800

    Add files via upload
    A simple rust file that prints the traditional "Hello World". (Instructions in code how to run)

---
+++ main.rs
@@ -0,0 +1,9 @@
+// Filename: main.rs 
+// Author: Kyle Shanahan
+// To run the code, 1) Open bash terminal 2) Type "$ rustc main.rs" 3) Type "$ ./main"
+// The message "Hello World" will be printed to the terminal
+ 
+ 
+fn main() {
+    println!("Hello, World!");
+}
